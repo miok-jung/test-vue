506a11ce0bd3e08d30d6c6e6b0b21ae7e3c7f639
miok-jung <miok.dev@gmail.com>
2024-03-15 17:29:02 +0900

    update: Add to accodion layout example2

--- src/components/TestComponent2.vue
+++ src/components/TestComponent2.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="q-pa-md">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sunt in fuga
-    laudantium, reiciendis unde ea qui illum consectetur, numquam enim eius eos
-    voluptate ipsa nostrum. Corrupti saepe illum iste!
+    <template v-for="n in 10" :key="n">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sunt in
+        fuga laudantium, reiciendis unde ea qui illum consectetur, numquam enim
+        eius eos voluptate ipsa nostrum. Corrupti saepe illum iste!
+      </p>
+      <br /><br /><br />
+    </template>
   </div>
 </template>
 
