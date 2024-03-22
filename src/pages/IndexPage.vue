8426d6c72b969d13d0fb14260c4025d2ff3ef79b
miok-jung <miok.dev@gmail.com>
2024-03-22 15:39:23 +0900

    feat: 기본 CKEditor 추가

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>Index</div>
+  <div>
+    <h1>Index</h1>
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const editor = ClassicEditor;
+
+const editorData = '<p>Content of the editor.</p>';
+const editorConfig = {
+  // The configuration of the editor.
+};
+</script>
 
 <style scoped></style>
